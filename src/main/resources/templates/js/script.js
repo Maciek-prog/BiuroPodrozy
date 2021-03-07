a83ee7909987f3e89b74298d11e316e6cfebfa70
Maciek-prog <pretkimaciej1@gmail.com>
2021-03-07 14:00:50 +0100

    narazie zakomentowałem caly slaider jeszcze poszukam rozwiązanie do niego wieczorem :)

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,8 +1,7 @@
-
-    (function () {
-    new Slider("#slider .slide").options({
-        prev: "#slider nav button.arrow-prev",
-        next: "#slider nav button.arrow-next",
-        nav: "#slider nav.dots span"
-    }).init()
-})();
+// (function () {
+//     new Slider("#slider" , ".slide").options({
+//         prev: "#slider nav button.arrow-prev",
+//         next: "#slider nav button.arrow-next",
+//         nav: "#slider nav.dots span"
+//     }).init();
+// });
