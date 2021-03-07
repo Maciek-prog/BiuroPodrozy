8879f3c95f8ed48265361464577a4c235f7c6f34
Maciek-prog <pretkimaciej1@gmail.com>
2021-03-07 11:56:16 +0100

    Zabawa w css i pokolorowanie wstępne żeby widzieć szablon strony, później starać się zrobić slajder ze zdjęć

---
+++ src/main/resources/templates/js/script.js
@@ -0,0 +1,26 @@
+// <ul>
+//     <input type="checkbox" id="zazn">
+//         <label htmlFor="zazn">
+//             <li>Azja</li>
+//         </label>
+//         <input type="checkbox">
+//             <label htmlFor="zazn">
+//                 <li>Ameryka Północna</li>
+//             </label>
+//             <input type="checkbox">
+//                 <label htmlFor="zazn">
+//                     <li>Ameryka Południowa</li>
+//                 </label>
+//                 <input type="checkbox">
+//                     <label htmlFor="zazn">
+//                         <li>Afryka</li>
+//                     </label>
+//                     <input type="checkbox">
+//                         <label htmlFor="zazn">
+//                             <li>Australia i Oceania</li>
+//                         </label>
+//                         <input type="checkbox">
+//                             <label htmlFor="zazn">
+//                                 <li>Europa</li>
+//                             </label>
+// </ul>
