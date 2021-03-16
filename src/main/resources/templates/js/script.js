ff3bc782644ed719e93e93e884e4fdc13048a3b1
Maciek-prog <pretkimaciej1@gmail.com>
2021-03-16 11:51:22 +0100

    Slider udany :) już jakoś to wygląda

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,7 +1,54 @@
-// (function () {
-//     new Slider("#slider" , ".slide").options({
-//         prev: "#slider nav button.arrow-prev",
-//         next: "#slider nav button.arrow-next",
-//         nav: "#slider nav.dots span"
-//     }).init();
-// });
+const slideList = [{
+    img: "Images/australia.jpg",
+    text: 'Australia'
+    },
+    {
+        img: "Images/Azja.jpg" ,
+        text: 'Azja'
+    },
+    {
+        img: "Images/barcelona.jpg",
+        text: 'Hiszpania'
+    },
+    {
+        img: "Images/LA.jpg",
+        text: 'Ameryka Północna'
+    },
+    {
+        img: "Images/oceania.jpg",
+        text: 'Oceania'
+    },
+    {
+        img: "Images/paryz.jpg",
+        text: 'Francja'
+    },
+    {
+        img: "Images/rio.jpg",
+        text: 'Ameryka Południowa'
+    }];
+
+const image = document.querySelector('img.slider');
+const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')]
+// Interfejs
+const time = 4000;
+let active = 0;
+
+// Implementacje
+
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+const changeSlide = () => {
+    active++;
+    if (active === slideList.length) {
+        active = 0;
+    }
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    changeDot()
+}
+setInterval(changeSlide, time)
